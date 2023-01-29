6c032f509536bf5e6f140000bb6348a8a503be6c
Ananth Murugan G K <ananthmgk4002@gmail.com>
2023-01-29 21:36:30 +0530

    created Event Capture, Propagation, Bubbling and Once.

---
+++ learned.md
@@ -0,0 +1,5 @@
+`e.stopPropagation()` = this fuction will stop bubbling(Propagation).
+by default the bubbling will starts from dowm to up.
+But when `capture: true`, the bubbling will starts from up to down.
+and when `capture: false`, the bubbling will starts from dowm to up.
+`once: true` the event will listen only once.
---
+++ main.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+  const divs = document.querySelectorAll('div');
+
+  function logText(e) {
+    console.log(this.classList.value);
+    // e.stopPropagation();
+  }
+
+  // document.body.addEventListener('click', logText);
+
+  divs.forEach((div) =>
+    div.addEventListener('click', logText, {
+      capture: false,
+      once: true,
+    })
+  );
+});
